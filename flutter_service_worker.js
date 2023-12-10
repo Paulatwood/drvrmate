ce313a83195a924dbf5cbd691e7045f0acff2af6
Paulatwood <Paulatwood1@gmail.com>
2023-12-10 13:10:10 +1000

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "15235b5108d6a877ef74fe3317a96bf7",
-"index.html": "faec4fad58e0080afb4216401b143277",
-"/": "faec4fad58e0080afb4216401b143277",
-"main.dart.js": "75eed0d6535d638a91ff03e28616920a",
+"index.html": "8b9d42c32b299d297a9158a658f5c01b",
+"/": "8b9d42c32b299d297a9158a658f5c01b",
+"main.dart.js": "94e0222ade04d02723dfeb103325183a",
 "flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
